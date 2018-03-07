89c5ed506e82928ce05505d74308042ae4463eb2
Marcelo Atie <marceloatie@gmail.com>
2018-03-07 16:08:13 -0400

    Update cleaninstall-42.3.sh

--- cleaninstall-42.3.sh
+++ cleaninstall-42.3.sh
@@ -10,14 +10,14 @@ zypper refresh
 # - Add main repositories
 zypper --quiet --non-interactive --gpg-auto-import-keys addrepo -p 99 http://download.opensuse.org/distribution/leap/42.3/repo/oss/suse/ 'openSUSE'
 zypper --quiet --non-interactive --gpg-auto-import-keys addrepo -p 99 http://download.opensuse.org/distribution/leap/42.3/repo/non-oss/suse/ 'openSUSE non-oss'
-zypper --quiet --non-interactive --gpg-auto-import-keys addrepo -p 99 http://download.opensuse.org/update/leap/42.3/oss/ 'openSUSE update'
-zypper --quiet --non-interactive --gpg-auto-import-keys addrepo -p 99 http://download.opensuse.org/update/leap/42.3/non-oss/ 'openSUSE update non-oss'
+zypper --quiet --non-interactive --gpg-auto-import-keys addrepo --refresh -p 99 http://download.opensuse.org/update/leap/42.3/oss/ 'openSUSE update'
+zypper --quiet --non-interactive --gpg-auto-import-keys addrepo --refresh -p 99 http://download.opensuse.org/update/leap/42.3/non-oss/ 'openSUSE update non-oss'
 
 # - Add third part repositories
-zypper --quiet --non-interactive --gpg-auto-import-keys addrepo -p 90 http://packman.inode.at/suse/openSUSE_Leap_42.3/ 'packman'
-zypper --quiet --non-interactive --gpg-auto-import-keys addrepo -p 80 https://dl.google.com/linux/rpm/stable/x86_64/ 'google-chrome'
-zypper --quiet --non-interactive --gpg-auto-import-keys addrepo -p 80 http://download.opensuse.org/repositories/home:/marceloatie/openSUSE_Leap_42.3/ 'telegram'
-zypper --quiet --non-interactive --gpg-auto-import-keys addrepo -p 80 http://download.opensuse.org/repositories/mozilla/openSUSE_Leap_42.3/ 'mozilla'
+zypper --quiet --non-interactive --gpg-auto-import-keys addrepo --refresh -p 90 http://packman.inode.at/suse/openSUSE_Leap_42.3/ 'packman'
+zypper --quiet --non-interactive --gpg-auto-import-keys addrepo --refresh -p 80 https://dl.google.com/linux/rpm/stable/x86_64/ 'google-chrome'
+zypper --quiet --non-interactive --gpg-auto-import-keys addrepo --refresh -p 80 http://download.opensuse.org/repositories/home:/marceloatie/openSUSE_Leap_42.3/ 'telegram'
+zypper --quiet --non-interactive --gpg-auto-import-keys addrepo --refresh -p 80 http://download.opensuse.org/repositories/mozilla/openSUSE_Leap_42.3/ 'mozilla'
 
 # - Install new software
 zypper refresh
