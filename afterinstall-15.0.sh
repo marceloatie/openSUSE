a2adf98cf426db049aa9469376931d05647afdc7
Marcelo Atie <marceloatie@gmail.com>
2018-03-23 17:19:36 -0400

    Included falkon and latte-dock

--- afterinstall-15.0.sh
+++ afterinstall-15.0.sh
@@ -16,7 +16,8 @@ zypper removerepo http://download.opensuse.org/source/distribution/leap/15.0/rep
 # - Add third part repositories
 zypper --quiet --non-interactive --gpg-auto-import-keys addrepo --refresh -p 90 http://packman.inode.at/suse/openSUSE_Leap_15.0/ 'packman'
 zypper --quiet --non-interactive --gpg-auto-import-keys addrepo --refresh -p 80 https://dl.google.com/linux/rpm/stable/x86_64/ 'google-chrome'
-zypper --quiet --non-interactive --gpg-auto-import-keys addrepo --refresh -p 80 http://download.opensuse.org/repositories/home:/marceloatie/openSUSE_Leap_15.0/ 'telegram'
+zypper --quiet --non-interactive --gpg-auto-import-keys addrepo --refresh -p 80 http://download.opensuse.org/repositories/home:/marceloatie/openSUSE_Leap_15.0/ 'home:marceloatie'
+zypper --quiet --non-interactive --gpg-auto-import-keys addrepo --refresh -p 90 http://download.opensuse.org/repositories/home:/opensuseitalia/openSUSE_Leap_15.0/ 'home:opensuseitalia'
 zypper --quiet --non-interactive --gpg-auto-import-keys addrepo --refresh -p 80 http://download.opensuse.org/repositories/mozilla/openSUSE_Leap_15.0/ 'mozilla'
 
 # - Install new software
@@ -24,3 +25,5 @@ zypper refresh
 zypper --non-interactive dist-upgrade --allow-vendor-change --from packman
 zypper --non-interactive dist-upgrade --allow-vendor-change --from mozilla
 zypper --non-interactive install telegram google-chrome-stable vlc vlc-codecs qbittorrent fuse-exfat exfat-utils
+# 15.0 / KDE stuff!
+zypper --non-interactive install falkon latte-dock 
