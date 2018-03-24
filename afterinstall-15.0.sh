b740a1da726044b3c330cbceb1c5f441a54e2b4b
Marcelo Atie <marceloatie@gmail.com>
2018-03-24 15:49:51 -0400

    Fix --gpg-auto-import-keys

--- afterinstall-15.0.sh
+++ afterinstall-15.0.sh
@@ -16,14 +16,14 @@ zypper removerepo http://download.opensuse.org/source/distribution/leap/15.0/rep
 zypper removerepo openSUSE-Leap-15.0-1
 
 # - Add third part repositories
-zypper --quiet --non-interactive --gpg-auto-import-keys addrepo --refresh -p 90 http://packman.inode.at/suse/openSUSE_Leap_15.0/ 'packman'
-zypper --quiet --non-interactive --gpg-auto-import-keys addrepo --refresh -p 80 https://dl.google.com/linux/rpm/stable/x86_64/ 'google-chrome'
-zypper --quiet --non-interactive --gpg-auto-import-keys addrepo --refresh -p 80 http://download.opensuse.org/repositories/home:/marceloatie/openSUSE_Leap_15.0/ 'home:marceloatie'
-zypper --quiet --non-interactive --gpg-auto-import-keys addrepo --refresh -p 90 http://download.opensuse.org/repositories/home:/opensuseitalia/openSUSE_Leap_15.0/ 'home:opensuseitalia'
-zypper --quiet --non-interactive --gpg-auto-import-keys addrepo --refresh -p 80 http://download.opensuse.org/repositories/mozilla/openSUSE_Leap_15.0/ 'mozilla'
+zypper --quiet --non-interactive addrepo --refresh -p 90 http://packman.inode.at/suse/openSUSE_Leap_15.0/ 'packman'
+zypper --quiet --non-interactive addrepo --refresh -p 80 https://dl.google.com/linux/rpm/stable/x86_64/ 'google-chrome'
+zypper --quiet --non-interactive addrepo --refresh -p 80 http://download.opensuse.org/repositories/home:/marceloatie/openSUSE_Leap_15.0/ 'home:marceloatie'
+zypper --quiet --non-interactive addrepo --refresh -p 90 http://download.opensuse.org/repositories/home:/opensuseitalia/openSUSE_Leap_15.0/ 'home:opensuseitalia'
+zypper --quiet --non-interactive addrepo --refresh -p 80 http://download.opensuse.org/repositories/mozilla/openSUSE_Leap_15.0/ 'mozilla'
+zypper --gpg-auto-import-keys refresh
 
 # - Install new software
-zypper refresh
 zypper --non-interactive dist-upgrade --allow-vendor-change --from packman
 zypper --non-interactive dist-upgrade --allow-vendor-change --from mozilla
 zypper --non-interactive install telegram google-chrome-stable vlc vlc-codecs qbittorrent fuse-exfat exfat-utils
