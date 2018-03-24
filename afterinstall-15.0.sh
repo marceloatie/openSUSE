a2c60aeafcb204aaf1ff543190846397553fe6ac
Marcelo Atie <marceloatie@gmail.com>
2018-03-24 14:12:16 -0400

    Check for codecs

--- afterinstall-15.0.sh
+++ afterinstall-15.0.sh
@@ -27,3 +27,5 @@ zypper --non-interactive dist-upgrade --allow-vendor-change --from mozilla
 zypper --non-interactive install telegram google-chrome-stable vlc vlc-codecs qbittorrent fuse-exfat exfat-utils
 # 15.0 / KDE stuff!
 zypper --non-interactive install falkon latte-dock yakuake
+#check for codecs
+zypper --non-interactive install libgstadaptivedemux-1_0-0 libgstbadaudio-1_0-0 libgstbadbase-1_0-0 libgstbadvideo-1_0-0 libgstbasecamerabinsrc-1_0-0 libgstcodecparsers-1_0-0 libgstgl-1_0-0 libgstmpegts-1_0-0 libgstphotography-1_0-0 libgsturidownloader-1_0-0 libgstwayland-1_0-0 libquicktime0 libsox3 sox
