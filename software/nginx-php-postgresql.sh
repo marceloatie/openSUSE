95da3f243ccd401e0f47402f8feae0a4d77d0e23
Marcelo Atie <marceloatie@gmail.com>
2018-03-30 22:13:24 -0400

    PHP basic installation

--- software/nginx-php-postgresql.sh
+++ software/nginx-php-postgresql.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+# - NGINX
 zypper in nginx
 systemctl enable nginx
 systemctl start nginx
+
+# - PHP
+zypper in php php-fpm
+cp /etc/php7/fpm/php-fpm.conf.default /etc/php7/fpm/php-fpm.conf
+cp /etc/php7/fpm/php-fpm.d/www.conf.default /etc/php7/fpm/php-fpm.d/www.conf
+#user = nobody -> user = nginx
+sed -i -- 's/user = nobody/\nuser = nginx/g' /etc/php7/fpm/php-fpm.d/www.conf
+#group = nobody -> group = nginx
+sed -i -- 's/group = nobody/group = nginx/g' /etc/php7/fpm/php-fpm.d/www.conf
+
+systemctl enable php-fpm
+systemctl start php-fpm
