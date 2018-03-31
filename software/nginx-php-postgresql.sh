4324a3431bfceb729ba4d9baac7ab07df628698b
Marcelo Atie <marceloatie@gmail.com>
2018-03-31 14:06:17 -0400

    postgresql stuff

--- software/nginx-php-postgresql.sh
+++ software/nginx-php-postgresql.sh
@@ -10,7 +10,7 @@ zypper in php php-fpm
 cp /etc/php7/fpm/php-fpm.conf.default /etc/php7/fpm/php-fpm.conf
 cp /etc/php7/fpm/php-fpm.d/www.conf.default /etc/php7/fpm/php-fpm.d/www.conf
 #user = nobody -> user = nginx
-sed -i -- 's/user = nobody/\nuser = nginx/g' /etc/php7/fpm/php-fpm.d/www.conf
+sed -i -- 's/user = nobody/user = nginx/g' /etc/php7/fpm/php-fpm.d/www.conf
 #group = nobody -> group = nginx
 sed -i -- 's/group = nobody/group = nginx/g' /etc/php7/fpm/php-fpm.d/www.conf
 
@@ -22,5 +22,15 @@ zypper in php-pgsql
 
 #PostgreSQL
 zypper in postgresql-server
+
+#ident -> password
+sed -i -- 's/host    all             all             127.0.0.1/32            ident/host    all             all             127.0.0.1\/32            password/g' /etc/postgresql/8.4/main/pg_hba.conf
+sed -i -- 's/host    all             all             ::1/128                 ident/host    all             all             ::1/128                 password/g' /etc/postgresql/8.4/main/pg_hba.conf
+
 systemctl enable postgresql
 systemctl start postgresql
+#user pg, password pg
+su - postgres -c "psql -U postgres -d postgres -c \"CREATE USER pg SUPERUSER CREATEDB CREATEROLE INHERIT LOGIN PASSWORD 'pg';\""
+
+#now you can connect using pg user trouth localhost using -> psql -h localhost -U pg -W postgres
+#but you can use psql directely using -> sudo -u postgres psql 
