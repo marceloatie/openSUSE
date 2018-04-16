d352b2dcedb47441ede5d78414c45a10cb8ad80b
Marcelo Atie <marceloatie@gmail.com>
2018-04-16 19:58:25 -0400

    Include TeamViewer

--- afterinstall-15.0.sh
+++ afterinstall-15.0.sh
@@ -29,5 +29,7 @@ zypper --non-interactive dist-upgrade --allow-vendor-change --from mozilla
 zypper --non-interactive install telegram google-chrome-stable vlc vlc-codecs qbittorrent fuse-exfat exfat-utils unar axel nmap imagewriter
 # 15.0 / KDE stuff!
 zypper --non-interactive install falkon latte-dock yakuake
-#check for codecs
+# check for codecs
 zypper --non-interactive install libgstadaptivedemux-1_0-0 libgstbadaudio-1_0-0 libgstbadbase-1_0-0 libgstbadvideo-1_0-0 libgstbasecamerabinsrc-1_0-0 libgstcodecparsers-1_0-0 libgstgl-1_0-0 libgstmpegts-1_0-0 libgstphotography-1_0-0 libgsturidownloader-1_0-0 libgstwayland-1_0-0 libquicktime0 libsox3 sox vlc-codec-gstreamer
+# TeamViewer
+zypper --non-interactive --no-gpg-check install https://download.teamviewer.com/download/linux/teamviewer-suse.x86_64.rpm
